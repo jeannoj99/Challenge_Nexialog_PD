bff0e62e8186b9cf7386ff19e93db8ae99c9b087
alice-l1 <146172085+alice-l1@users.noreply.github.com>
2024-04-17 21:30:18 +0200

    update page ml model

--- web_app/pages/ml_models.py
+++ web_app/pages/ml_models.py
@@ -15,9 +15,22 @@ from utils.utils_from_cecile import subplot_segment_default_rate, show_risk_stab
 from utils.callbacks_j import pd_summary_ml_cash, pd_summary_ml_revolving, pd_summaries_ml, ginis_ml_models, models_challenger, datas_challenger, shap_graphs_src, update_interpretable_ml, update_risk_quantification_ml, toggle_modal
 
 
+border_color = "#8C8C8C"
+
+style = {
+    #"height": 100,
+    "border": f"1px solid {border_color}",
+    "marginTop": 20,
+    #"marginBottom": 20,
+    "borderRadius": 10,  # Arrondir les bordures
+    "backgroundColor": "white",  # Fond blanc
+    
+}
+
 # Define the layout of the app
 layout = html.Div([
     dmc.Title("Modèles de Machine Learning", order=1, style={'textAlign': 'center', 'marginBottom': '10px'}),
+    html.Br(),
     dcc.Dropdown(
         id='model-choice',
         options=[
@@ -43,12 +56,16 @@ layout = html.Div([
 ),
 
     html.Div([
-        dmc.Title("Interprétation du modèle", order=3, style={'textAlign': 'center', 'marginBottom': '10px'}),
+
+        html.Div([
+        dmc.Title("Interprétation du modèle", order=2, style={'textAlign': 'center', 'marginBottom': '10px'}),
         html.Div([
             dcc.Graph(id='feature-importances'),
             html.Img(id='shap-image', src='/assets/shap_summary_plot_cash.png', style={'width': 'auto', 'height': '500'})
         ], style={'display': 'flex', 'justifyContent': 'space-around'})  # Graphs side by side
-    ], style={'padding': '20px', 'border': '1px solid #ddd', 'borderRadius': '5px', 'margin': '20px'}),
+    ], style={'padding': '20px', 
+            #   'border': '1px solid #ddd', 
+              'borderRadius': '5px', 'margin': '20px'}),
 
 
     dbc.Card(
@@ -58,11 +75,16 @@ layout = html.Div([
             style={'textAlign': 'center', 'fontWeight': 'bold'}  # Texte en gras
         )],
         body=True,
-        style={'maxWidth': '200px', 'margin': '20px auto', 'padding': '10px','backgroundColor': 'skyblue'}
-    ),
+        style={'maxWidth': '200px', 'margin': '20px auto', 'padding': '10px', 'backgroundColor': 'skyblue'}
+    )
+ 
+    ] , style={**style, 'borderRadius': 10, 'backgroundColor': 'white'}),
+
 
     html.Div([
-        html.H2("Quantification du risque", style={'textAlign': 'center'}),
+
+           html.Div([
+        dmc.Title("Quantification du risque", order=2, style={'textAlign': 'center', 'marginBottom': '10px'}),
         html.Div([
             dcc.Graph(id="risk-stability-overtime"),
             dcc.Graph(id='density-plot-challenger')
@@ -72,8 +94,16 @@ layout = html.Div([
             dcc.Graph(id='segment-challenger')  # Third graph below and centered
         ], style={'width': '60%', 'margin': '0 auto'}),
         
+   
+    ], style={'padding': '20px', 'borderRadius': '5px', 'margin': '20px'})
+        
+
+    ], style={**style, 'borderRadius': 10, 'backgroundColor': 'white'}),
+
     html.Div([
-            dmc.Title("Tableau résumé de la PD", order=3 , style={'textAlign': 'center', 'marginBottom': '10px'}),
+
+         html.Div([
+            dmc.Title("Tableau résumé de la PD", order=2 , style={'textAlign': 'center', 'marginBottom': '10px'}),
             dash_table.DataTable(
         id='pd-summary',
         style_table={'overflowX': 'auto'},
@@ -92,7 +122,42 @@ layout = html.Div([
         ],
         style_as_list_view=True
 )])
-    ], style={'padding': '20px', 'border': '1px solid #ddd', 'borderRadius': '5px', 'margin': '20px'})
+
+    ], style={**style, 'borderRadius': 10, 'backgroundColor': 'white'}),
+
+
+#     html.Div([
+#         html.H2("Quantification du risque", style={'textAlign': 'center'}),
+#         html.Div([
+#             dcc.Graph(id="risk-stability-overtime"),
+#             dcc.Graph(id='density-plot-challenger')
+#         ], style={'display': 'flex', 'justifyContent': 'space-around'}),  # Two graphs side by side
+        
+#         html.Div([
+#             dcc.Graph(id='segment-challenger')  # Third graph below and centered
+#         ], style={'width': '60%', 'margin': '0 auto'}),
+        
+#     html.Div([
+#             dmc.Title("Tableau résumé de la PD", order=3 , style={'textAlign': 'center', 'marginBottom': '10px'}),
+#             dash_table.DataTable(
+#         id='pd-summary',
+#         style_table={'overflowX': 'auto'},
+#         style_cell={
+#             'minWidth': '50px',  # Réduire la largeur minimum pour s'adapter mieux au contenu
+#             'width': 'auto',     # Laisser la largeur s'ajuster automatiquement
+#             'maxWidth': 'auto', # Limite supérieure de la largeur
+#             'whiteSpace': 'normal'
+#         },
+#         style_header={
+#             'backgroundColor': 'rgb(230, 230, 230)',
+#             'fontWeight': 'bold'
+#         },
+#         style_data_conditional=[
+#             {'if': {'row_index': 'odd'}, 'backgroundColor': 'rgb(248, 248, 248)'}
+#         ],
+#         style_as_list_view=True
+# )])
+#     ], style={'padding': '20px', 'border': '1px solid #ddd', 'borderRadius': '5px', 'margin': '20px'})
 ])
 
 
